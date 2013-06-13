d9bb82527607cb431ea4c95b40fd0343acc261bc
Trevor Bedford <trvrb@trevorbedford.com>
2013-06-13 15:17:02 -0700

    Remove BEAST package.

--- modules/people/manifests/trvrb.pp
+++ modules/people/manifests/trvrb.pp
@@ -10,18 +10,18 @@ class people::trvrb {
   include java
   include vlc  
   include transmit  
-
-  # programs
-  package { 'beast':
-    source => 'trvrb/homebrew-science/beast-1.7.5/beast-1.7.5',
-    ensure => installed,
-    install_options => ['--HEAD']
-  }
   
   # ruby
   class { 'ruby::global':
     version => '1.9.3'
   }  
+
+  # programs
+#  package { 'beast':
+#    source => 'trvrb/homebrew-science/beast-1.7.5/beast-1.7.5',
+#    ensure => installed,
+#    install_options => ['--HEAD']
+#  }
     
 # I believe this is throwing an error because a non-HEAD version of BEAGLE does not exist in the formula 
 #  package { 'beagle':
